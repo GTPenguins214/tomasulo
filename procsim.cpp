124de8ba392017726503b9f3a481858d7c155c5f
Brenton Yiu <brenton.yiu@gatech.edu>
2014-06-26 23:34:12 -0400

    Added some fields to the instruction structure. Created the frame work for the methods.

--- procsim.cpp
+++ procsim.cpp
@@ -1,4 +1,12 @@
 #include "procsim.hpp"
+#include <iostream>
+
+using namespace std;
+
+#define DEBUG 0
+
+proc_info_t info;
+reg_file_t reg_file[8];
 
 /**
  * Subroutine for initializing the processor. You many add and initialize any global or heap
@@ -14,6 +22,40 @@
  * @c Number of CDBs
  */
 void setup_proc(uint64_t d, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f, uint64_t m, uint64_t c) {
+	// Populate the information structure
+	info.dispatch_q_mult = (int) d;
+	info.num_k0 = (int) k0;
+	info.num_k1 = (int) k1;
+	info.num_k2 = (int) k2;
+	info.fetch_rate = (int) f;
+	info.schedule_q_mult = (int) m;
+	info.num_cdb = (int) c;
+
+	if (DEBUG) {
+		std::cout << "Num K0: " << info.num_k0 << '\n';
+		std::cout << "Num K1: " << info.num_k1 << '\n';
+		std::cout << "Num K2: " << info.num_k2 << '\n';
+		std::cout << "Fetch Rate: " << info.fetch_rate << '\n';
+		std::cout << "Dispatch Queue Multiplier: " << info.dispatch_q_mult << '\n';
+		std::cout << "Schedule Queue Multiplier: " << info.schedule_q_mult << '\n';
+		std::cout << "Num CDB: " << info.num_cdb << "\n\n";
+	}
+
+	// Set up the register file
+	for (int i = 0; i < 8; i++) {
+		reg_file[i].ready = true;
+		reg_file[i].tag = -1;
+		reg_file[i].value = -1;
+
+		if (DEBUG) {
+			std::cout << "[" << i << "].ready: " << reg_file[i].ready << '\n';
+			std::cout << "[" << i << "].tag: " << reg_file[i].tag << '\n';
+			std::cout << "[" << i << "].value: " << reg_file[i].value << "\n\n";
+		}
+	}
+
+	// Print the headings
+	printf("INST\tFETCH\tDISP\tSCHEDU\t EXEC\tSTATE\n");
 }
 
 /**
@@ -35,4 +77,25 @@ void run_proc(proc_stats_t* p_stats) {
  * @p_stats Pointer to the statistics structure
  */
 void complete_proc(proc_stats_t *p_stats) {
+
+}
+
+void fetch() {
+
+}
+
+void dispatch() {
+
+}
+
+void schedule() {
+
+}
+
+void execute() {
+
+}
+
+void state_update() {
+	
 }
--- procsim.hpp
+++ procsim.hpp
@@ -19,6 +19,17 @@ typedef struct _proc_inst_t
     int32_t dest_reg;
     
     // You may introduce other fields as needed
+
+    // These are used to keep track of the cycle in which the instruction
+    // first got into that stage. It's used for the output
+    int fetch_cycle;
+    int dispatch_cycle;
+    int schedule_cycle;
+    int execute_cycle;
+    int state_cycle;
+
+    // Used when we need to tag a register
+    int instruction_num;
     
 } proc_inst_t;
 
@@ -36,5 +47,25 @@ bool read_instruction(proc_inst_t* p_inst);
 void setup_proc(uint64_t d, uint64_t k0, uint64_t k1, uint64_t k2, uint64_t f, uint64_t m, uint64_t c);
 void run_proc(proc_stats_t* p_stats);
 void complete_proc(proc_stats_t* p_stats);
+
+// This holds the information that was sent in through the command-line
+typedef struct _proc_info_t {
+    int dispatch_q_mult;
+    int fetch_rate;
+    int schedule_q_mult;
+    int num_k0;
+    int num_k1;
+    int num_k2;
+    int num_cdb;
+    // Keep track of the instruction number that is being read
+    int inst_num; 
+} proc_info_t;
+
+// Entries for the register file
+typedef struct _reg_file_t {
+    bool ready;
+    int tag;
+    int value;
+} reg_file_t;
 
 #endif /* PROCSIM_HPP */
